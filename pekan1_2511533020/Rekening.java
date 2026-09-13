eaa79a7ea21680a683936c7f01953a50d3300729
shauqiakbar <jakasro555@gmail.com>
2026-09-13 23:50:18 +0700

    Add files via upload

---
+++ pekan1_2511533020/Rekening.java
@@ -0,0 +1,46 @@
+package pekan1;
+
+public class Rekening {
+    String nomorRekening;
+    String namaPemilik;
+    double saldo;
+
+    public Rekening(String nomor, String nama, double saldoAwal) {
+        nomorRekening = nomor;
+        namaPemilik = nama;
+        saldo = saldoAwal;
+        System.out.println("Rekening atas nama " + namaPemilik + " berhasil dibuat dengan saldo Rp" + saldo);
+    }
+
+    public void setorTunai(double nominal) {
+        if (nominal > 0) {
+            saldo += nominal;
+            System.out.println("Setor tunai Rp" + nominal + " berhasil. Saldo saat ini: Rp" + saldo);
+        } else {
+            System.out.println("Gagal: Nominal setor harus lebih dari 0!");
+        }
+    }
+
+    public void tarikTunai(double nominal) {
+        if (nominal < 10000) {
+            System.out.println("Transaksi Gagal: Minimal nominal penarikan Rp10000");
+        } else if (nominal > saldo) {
+            System.out.println("Transaksi Gagal: Saldo tidak mencukupi. Saldo Anda: Rp" + saldo);
+        } else {
+            saldo -= nominal;
+            System.out.println("Tarik tunai Rp" + nominal + " berhasil. Saldo saat ini: Rp" + saldo);
+        }
+    }
+
+    public boolean cocokDenganNomor(String nomor) {
+        return this.nomorRekening.equals(nomor);
+    }
+
+    public void cekInformasi() {
+        System.out.println("--- INFO REKENING ---");
+        System.out.println("No. Rekening : " + nomorRekening);
+        System.out.println("Nama Pemilik : " + namaPemilik);
+        System.out.println("Saldo Akhir  : Rp" + saldo);
+        System.out.println("----------------------");
+    }
+}
